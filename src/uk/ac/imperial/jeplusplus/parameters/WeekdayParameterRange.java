da3399f53f09e99665f865a4c80be1efa9e8308a
James Keirstead <james.keirstead@gmail.com>
2014-05-20 12:40:02 +0100

    Updated the documentation for the ParameterRange objects.

--- src/uk/ac/imperial/jeplusplus/parameters/WeekdayParameterRange.java
+++ src/uk/ac/imperial/jeplusplus/parameters/WeekdayParameterRange.java
@@ -1,10 +1,18 @@
 package uk.ac.imperial.jeplusplus.parameters;
 
+/**
+ * Describes a range of valid weekday values. Sunday is the first entry in the
+ * range.
+ * 
+ * @author James Keirstead
+ * 
+ */
 public class WeekdayParameterRange extends ParameterRange<String> {
 
 	@Override
 	public String[] getValues() {
-		String[] weekdays = {"Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"};
+		String[] weekdays = { "Sunday", "Monday", "Tuesday", "Wednesday",
+				"Thursday", "Friday", "Saturday" };
 		return weekdays;
 	}
 
