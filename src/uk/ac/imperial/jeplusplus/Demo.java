9ba41abd7e44698d867df946c1d064b6a3a1ed88
James Keirstead <james.keirstead@gmail.com>
2014-05-22 18:41:11 +0100

    Added a basic demo application.

---
+++ src/uk/ac/imperial/jeplusplus/Demo.java
@@ -0,0 +1,34 @@
+package uk.ac.imperial.jeplusplus;
+
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
+/**
+ * A simple demostration of jE++
+ * 
+ * @author James Keirstead
+ *
+ */
+public class Demo {
+	
+	// Set your path to jEPlus here
+	private static String JAR_PATH = "d:/software/jEPlus_v1.5_pre_05/jEPlus.jar";
+	
+	public static void main(String[] args) throws IOException {
+		
+		// Define the input and output directories
+		Path input = Paths.get("demo");
+		Path output = input.resolve("output");
+		
+		// Tell jE++ where jEPlus lives
+		JEPlusController.setJarPath(JAR_PATH);
+		
+		// Build the project
+		JEPlusProject project = new JEPlusProject(input, output);
+		project.run();
+		
+	}
+
+}
